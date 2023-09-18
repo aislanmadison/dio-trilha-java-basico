3c1e6722defeab835d9f5c4a49d23b419bf71634
aislanmadison <aislanmadison@hotmail.com>
2023-09-18 17:29:49 -0300

    Impelemtação de recursos aula 18-09

---
+++ controle-de-fluxo/controle-de-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,14 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+     double saldo = 25.0;
+     double valorSolicitado = 22.0;
+
+     if (valorSolicitado < saldo)
+        saldo = saldo - valorSolicitado;
+     else
+        System.out.println("Saldo insuficiente");
+        System.out.println("Saldo atual: R$ " + saldo);
+
+    }
+}
+    
