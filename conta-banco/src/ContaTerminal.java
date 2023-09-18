b26a87dd6032bafc4bff4920ff1ebeab62aed4dd
aislanmadison <aislanmadison@hotmail.com>
2023-09-18 12:24:08 -0300

    conclusão de desafio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,36 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+
+        // Definindo os valores das constantes
+        int numero = 1012;
+        int agencia = 1893;
+        String nomeCliente = "Aislan Madison";
+        double saldo = 12025.34;
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o número da conta");
+        int numero2 = scanner.nextInt();
+
+        System.out.println("Digite o número da Agência");
+        int agencia2 = scanner.nextInt();
+    
+        if (numero==numero2 && agencia==agencia2){
+    
+        //Mensagem a ser exibida
+        System.out.println("Olá " +nomeCliente);
+        System.out.println("Conta: "+ numero);
+        System.out.println("Agência: "+ agencia);
+        System.out.println("Saldo: R$ " + saldo);
+    }
+
+        else{
+        System.out.println("Desculpe, alguma informação está incorreta. Tente novamente");
+        }
     }
 }
